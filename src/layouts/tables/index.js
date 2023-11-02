7ec058909e6a1c91a02927d6349b7fcaf1ebf2b6
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-11-03 01:12:56 +0530

    Restore user modal

--- src/layouts/tables/index.js
+++ src/layouts/tables/index.js
@@ -14,6 +14,7 @@ import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
 import ImportExportIcon from "@mui/icons-material/ImportExport";
 import { Close } from "@mui/icons-material";
+import {AiOutlineSearch } from "react-icons/ai";
 
 // Soft UI Dashboard React examples
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
@@ -63,6 +64,21 @@ const style = {
   maxHeight: "400px",
   overflowY: "scroll",
 };
+
+const styleTable = {
+  position: "absolute",
+  top: "30%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 800,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  maxHeight: "500px",
+  overflowY: "scroll",
+
+}
 const scrollableContentStyle = {
   overflowY: "auto",
   maxHeight: "300px",
@@ -147,6 +163,21 @@ function Tables() {
   const [groupItems, setGroupItems] = useState(["Technical"]);
   const [deleteUser, setDeleteUser] = useState(false);
   const [importUsersModal, setImportUsersModal] = useState(false);
+  const [restoreUser, setRestoreUser] = useState(false);
+  const [search, setSearch] = useState("");
+  
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value); 
+  };
+
+  const openRestoreUser = () => {
+    setRestoreUser(true);
+  };
+
+  const closeRestoreUser = () => {
+    setRestoreUser(false);
+  };
 
 
   const openImportUsersModal = () => {
@@ -360,6 +391,11 @@ const handleManagerChanges = (event) => {
     setGroupFormOpen(false);
   };
 
+  const handleRestoreClick = (row) => {
+    alert(`Restoring user: ${row.name}`);
+  };
+  
+
   const handleDeleteGroup = (itemToDelete) => (event) => {
     console.log(itemToDelete);
     event.preventDefault();
@@ -441,14 +477,108 @@ const handleManagerChanges = (event) => {
                   >
                     <AiOutlineReload />
                   </Button>
+
                   <Button
                     variant="outline"
                     style={{ border: "0.5px solid grey", color: "#585958", color: "#585958" }}
                     size="small"
-                    onClick={openUserForm}
+                    onClick={openRestoreUser}
                   >
                     Restore Users
                   </Button>
+
+                  <Modal open={restoreUser} onClose={closeRestoreUser}>
+                  <Box sx={styleTable}>
+                  <IconButton
+                    edge="end"
+                    color="inherit"
+                    onClick={closeRestoreUser}
+                    style={{ position: "absolute", right: 10, top: 10, zIndex: 1 }}
+                  >
+                    <Close />
+                  </IconButton>
+                    <Typography>Resore Users</Typography>
+                    <hr/>
+
+                    <Button
+                    variant="outline"
+                    style={{
+                      border: "0.5px solid #d2d6da",
+                      color: "#d4d4d4",
+                      fontSize: "10px",
+                      minWidth: 150,
+                      maxHeight: 40,
+                      marginTop:'15px'
+                    }}
+                    size="small"
+                  >
+                    Search for a user
+                    <AiOutlineSearch style={{ color: "grey", fontSize: "15px" }} />
+                  </Button>
+                    <TableContainer component={Paper} style={{marginTop:'20px'}}>
+                      <Table sx={{ width: "100%" }} aria-label="simple table">
+                        <TableHead sx={{ display: "table-header-group" }}>
+                          <TableRow sx={{ width: "20px" }}>
+                          <TableCell>Sl. No.</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Email ID</TableCell>
+                            <TableCell>Deleted At</TableCell>
+                            <TableCell>Restore</TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        <TableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell>1</TableCell>
+                      <TableCell style={{fontSize:"13px", color:"#209ce8"}}>harri loganathan</TableCell>
+                      <TableCell style={{fontSize:"13px"}}>harri@twintechsolution.com</TableCell>
+                      <TableCell style={{fontSize:"13px"}}>20/10/2023 16:57:39</TableCell>
+                      <TableCell>
+                        <button onClick={handleRestoreClick} style={{height:'30px', width:'100px', background:'#fff'}}>Restore User</button>
+                      </TableCell>
+                    </TableRow>
+
+                    <TableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell>2</TableCell>
+                      <TableCell style={{fontSize:"13px", color:"#209ce8"}}>harri loganathan</TableCell>
+                      <TableCell style={{fontSize:"13px"}}>harri@twintechsolution.com</TableCell>
+                      <TableCell style={{fontSize:"13px"}}>20/10/2023 16:57:39</TableCell>
+                      <TableCell>
+                        <button onClick={handleRestoreClick} style={{height:'30px', width:'100px', background:'#fff'}}>Restore User</button>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell>3</TableCell>
+                      <TableCell style={{fontSize:"13px", color:"#209ce8"}}>harri loganathan</TableCell>
+                      <TableCell style={{fontSize:"13px"}}>harri@twintechsolution.com</TableCell>
+                      <TableCell style={{fontSize:"13px"}}>20/10/2023 16:57:39</TableCell>
+                      <TableCell>
+                        <button onClick={handleRestoreClick} style={{height:'30px', width:'100px', background:'#fff'}}>Restore User</button>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell>4</TableCell>
+                      <TableCell style={{fontSize:"13px", color:"#209ce8"}}>harri loganathan</TableCell>
+                      <TableCell style={{fontSize:"13px"}}>harri@twintechsolution.com</TableCell>
+                      <TableCell style={{fontSize:"13px"}}>20/10/2023 16:57:39</TableCell>
+                      <TableCell>
+                        <button onClick={handleRestoreClick} style={{height:'30px', width:'100px', background:'#fff'}}>Restore User</button>
+                      </TableCell>
+                    </TableRow>
+                          
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                    </Box>
+                  </Modal>
+
                   <Dialog open={isUserFormOpen} onClose={closeUserForm}>
                     {/* User form content here */}
                   </Dialog>
