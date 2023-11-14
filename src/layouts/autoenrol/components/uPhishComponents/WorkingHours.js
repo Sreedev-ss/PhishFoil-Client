1c5d97cdf65573fb1bb366636c879f61a9530ae6
Akhila Vijayan <akhilavijayan964@gmail.com>
2023-11-14 15:22:20 +0530

    time piker added

--- src/layouts/autoenrol/components/uPhishComponents/WorkingHours.js
+++ src/layouts/autoenrol/components/uPhishComponents/WorkingHours.js
@@ -2,7 +2,13 @@ import { Typography, Switch, Button, TextField } from "@mui/material";
 import React, { useState } from "react";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import SaveIcon from "@mui/icons-material/Save";
-
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+// import { TimePicker } from "material-ui-time-picker";
+import TimePickerViewRenderers from "./TimePicker";
+import TimePickerViewRenderersTo from "./TimePickerTo";
 
 const WorkingHours = () => {
   const label = { inputProps: { "aria-label": "Switch demo" } };
@@ -40,8 +46,17 @@ const WorkingHours = () => {
           fontFamily: "sans-serif",
         }}
       >
-        Set your working hours
+        Set your working hours:
       </p>
+      <div>
+      <div>
+        <TimePickerViewRenderers />
+      </div>
+      <div>
+        <TimePickerViewRenderersTo />
+      </div>
+      </div>
+      
       <Button
         variant="contained"
         style={{
